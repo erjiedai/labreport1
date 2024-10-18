747168266e528c6e6ccbd3b720c34bd649ebe267
TIEGUODUNDAE <1492466460@qq.com>
2024-10-18 13:06:06 +0800

    Create lab16.v

---
+++ lab16/RTL/lab16.v
@@ -0,0 +1,18 @@
+
+
+module lab16(
+    input KEY1,
+    input KEY2,
+	 input KEY3,
+    output LED0,
+    output LED1,
+    output LED2,
+    output LED3
+    );
+ 
+assign LED0 = (~KEY3) & (~KEY2);
+assign LED1 = KEY2 & (~KEY3);
+assign LED2 = KEY3 & (~KEY2);
+assign LED3 = KEY3 & KEY2;
+ 
+endmodule
