d2b8d59bf83b9ee7ef826088b2f23e129f4fd8ec
TIEGUODUNDAE <1492466460@qq.com>
2024-10-21 12:16:48 +0800

    Update tb_lab16.v

--- lab16/Sim/tb_lab16.v
+++ lab16/Sim/tb_lab16.v
@@ -1,6 +1,12 @@
 `timescale 1ns / 1ns
+ // Define simulation parameter
+ // The first number is the time unit.
+ // The second number is the time precision.
+
  
-module tb_lab16();
+ 
+ // Define the testbench module
+ module tb_lab16();
 reg KEY1;
 reg KEY2;
 reg KEY3;
@@ -9,6 +15,7 @@ wire LED1;
 wire LED2;
 wire LED3;
  
+ //init input signal
 initial begin
     KEY1 <= 1'b0;
     KEY2 <= 1'b0;
@@ -16,13 +23,21 @@ initial begin
 	 
 end
  
+ 
+ 
+ 
+ // Generate inputs to simulate the button press
+// Every 10 ns, generate a random number. This random number will 
+// take the modulus and find the remainder to generate random 0 and 1 
  always #10 KEY1 <= {$random} % 2;
  always #10 KEY2 <= {$random} % 2;
  always #10 KEY3 <= {$random} % 2;
 
 initial begin
-$timeformat(-9, 0, "ns", 6);
+$timeformat(-9, 0, "ns", 6);// This part shows how to print values
+                           
 $monitor("@time %t:KEY1=%b KEY2=%b KEY3=%b LED3=%b LED2=%b LED1=%b LED0=%b",$time,KEY1,KEY2,KEY3,LED3,LED2,LED1,LED0);
+//When time, in1, in2, sel, or out are changed, the information will be displayed
 end
  
  lab16 lab16_inst (
