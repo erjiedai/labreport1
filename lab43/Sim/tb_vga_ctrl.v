7908a2cd9a00fce648e32396185e2a4c1914e936
TIEGUODUNDAE <1492466460@qq.com>
2024-11-20 20:33:10 +0800

    Update tb_vga_ctrl.v

--- lab43/Sim/tb_vga_ctrl.v
+++ lab43/Sim/tb_vga_ctrl.v
@@ -4,19 +4,19 @@
 module  tb_vga_ctrl();
 
 
-//wire  define
+
 wire            locked      ;
 wire            rst_n       ;
 wire            vga_clk     ;
 
-//reg   define
+
 reg             sys_clk     ;
 reg             sys_rst_n   ;
 reg     [15:0]  pix_data    ;
 
 
 
-//sys_clk,sys_rst_n初始赋值
+
 initial
     begin
         sys_clk     =   1'b1;
@@ -25,10 +25,10 @@ initial
         sys_rst_n   <=  1'b1;
     end
 
-//sys_clk：产生时钟
+
 always  #10 sys_clk = ~sys_clk  ;
 
-//rst_n:VGA模块复位信号
+//rst_n:VGA模块复位
 assign  rst_n = (sys_rst_n & locked);
 
 //pix_data:输入像素点色彩信息
@@ -42,24 +42,24 @@ always@(posedge vga_clk or  negedge rst_n)
 
 clk_gen clk_gen_inst
 (
-    .areset     (~sys_rst_n ),  //输入复位信号,高电平有效,1bit
-    .inclk0     (sys_clk    ),  //输入50MHz晶振时钟,1bit
-    .c0         (vga_clk    ),  //输出VGA工作时钟,频率25Mhz,1bit
-    .locked     (locked     )   //输出pll locked信号,1bit
+    .areset     (~sys_rst_n ),  //输入复位信号
+    .inclk0     (sys_clk    ),  //输入时钟
+    .c0         (vga_clk    ),  //输出VGA时钟
+    .locked     (locked     )   //输出pll locked信号
 );
 
 
 vga_ctrl  vga_ctrl_inst
 (
-    .vga_clk    (vga_clk    ),  //输入工作时钟,频率25MHz,1bit
-    .sys_rst_n  (rst_n      ),  //输入复位信号,低电平有效,1bit
-    .pix_data   (pix_data   ),  //输入像素点色彩信息,16bit
+    .vga_clk    (vga_clk    ),  //输入工作时钟
+    .sys_rst_n  (rst_n      ),  //输入复位信号,低电平有效
+    .pix_data   (pix_data   ),  //输入像素点色彩信息
 
-    .pix_x      (pix_x      ),  //输出VGA有效显示区域像素点X轴坐标,10bit
-    .pix_y      (pix_y      ),  //输出VGA有效显示区域像素点Y轴坐标,10bit
-    .hsync      (hsync      ),  //输出行同步信号,1bit
-    .vsync      (vsync      ),  //输出场同步信号,1bit
-    .rgb        (rgb        )   //输出像素点色彩信息,16bit
+    .pix_x      (pix_x      ),  //输出VGA有效显示区域像素点X轴坐标
+    .pix_y      (pix_y      ),  //输出VGA有效显示区域像素点Y轴坐标
+    .hsync      (hsync      ),  //输出行同步信号
+    .vsync      (vsync      ),  //输出场同步信号
+    .rgb        (rgb        )   //输出像素点色彩信息
 );
 
 endmodule
